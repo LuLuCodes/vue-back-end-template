54e5465e4ba50ec1ed924d9b13e0811f480b316d
qianqing <qian.qing@aliyun.com>
2017-07-15 17:53:51 +0800

    init

---
+++ src/views/Bashboard/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="dashboard-editor-container">
+    <el-row class="btn-group">
+      <el-col :span="4" class='text-center'>
+        <span class="pan-btn light-blue-btn">今日订单：123</span>
+      </el-col>
+      <el-col :span="4" class='text-center'>
+        <span class="pan-btn pink-btn">今日成交：123</span>
+      </el-col>
+      <el-col :span="4" class='text-center'>
+        <span class="pan-btn green-btn">今日新增客户：123</span>
+      </el-col>
+    </el-row>
+    
+    <el-row>
+      <el-col :span="8">
+        <money-chart></money-chart>
+      </el-col>
+      
+      <el-col :span="10">
+        <order-chart></order-chart>
+      </el-col>
+    </el-row>
+    
+    <el-row :gutter="20">
+      <el-col :span="24">
+        <visitor-chart></visitor-chart>
+      </el-col>
+    </el-row>
+  
+  </div>
+</template>
+
+<script>
+  import moneyChart from './moneyChart.vue';
+  import orderChart from './orderChart.vue';
+  import visitorChart from './visitorChart.vue';
+  export default {
+    name: 'dashboard',
+    components: { moneyChart, visitorChart, orderChart },
+    data() {
+      return {
+      };
+    },
+    computed: {
+    }
+  };
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .dashboard-editor-container {
+    margin: 30px;
+    .btn-group {
+      margin-bottom: 60px;
+    }
+    .box-card-header {
+      position: relative;
+      height: 160px;
+    }
+    .panThumb {
+      z-index: 100;
+      height: 150px;
+      width: 150px;
+      position: absolute;
+      left: 0px;
+      right: 0px;
+      margin: auto;
+    }
+    .display_name{
+      font-size: 30px;
+      display: block;
+    }
+    .info-item{
+      display: inline-block;
+      margin-top: 10px;
+      font-size: 14px;
+      &:last-of-type{
+        margin-left: 15px;
+      }
+    }
+  }
+</style>
---
+++ src/views/Bashboard/orderChart.vue
@@ -0,0 +1,76 @@
+<template>
+  <div :class="className" :style="{height:height,width:width}"></div>
+</template>
+
+<script>
+  import echarts from 'echarts';
+  require('echarts/theme/macarons'); // echarts 主题
+
+  export default {
+    props: {
+      className: {
+        type: String,
+        default: 'chart'
+      },
+      width: {
+        type: String,
+        default: '100%'
+      },
+      height: {
+        type: String,
+        default: '300px'
+      }
+    },
+    data() {
+      return {
+        chart: null
+      };
+    },
+    mounted() {
+      this.initChart();
+    },
+    beforeDestroy() {
+      if (!this.chart) {
+        return;
+      }
+      this.chart.dispose();
+      this.chart = null;
+    },
+    methods: {
+      initChart() {
+        this.chart = echarts.init(this.$el, 'macarons');
+
+        this.chart.setOption({
+          title: {
+            text: '本周订单进度',
+            x: 'center'
+          },
+          tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b} : {c} ({d}%)'
+          },
+          legend: {
+            x: 'center',
+            y: 'bottom',
+            data: ['已完成', '待付款', '待发货', '已发货', '已取消']
+          },
+          calculable: true,
+          series: [
+            {
+              name: '本周订单进度',
+              type: 'pie',
+              roseType: 'radius',
+              data: [
+                { value: 320, name: '已完成' },
+                { value: 240, name: '代付款' },
+                { value: 149, name: '待发货' },
+                { value: 100, name: '已发货' },
+                { value: 59, name: '已取消' }
+              ]
+            }
+          ]
+        });
+      }
+    }
+  };
+</script>
