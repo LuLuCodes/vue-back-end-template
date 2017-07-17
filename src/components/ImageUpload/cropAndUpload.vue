48f9ba340a213292d8ca97c0ceb3fb6cdebe67f9
qianqing <qian.qing@aliyun.com>
2017-07-17 14:42:28 +0800

    init

---
+++ src/components/ImageUpload/cropAndUpload.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="upload-container">
+    <el-upload class="image-uploader" drag :multiple="false" action="https://httpbin.org/post"
+               :on-success="handleImageScucess">
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+      <div class="el-upload__tip" slot="tip">建议尺寸: {{previewSize.width}} * {{previewSize.height}}，支持jpg/png文件，且不超过500kb</div>
+    </el-upload>
+
+    <div class="image-preview" :style="{ width: previewSize.width + 'px', height:  previewSize.height + 'px'}">
+      <div class="image-preview-wrapper" v-show="imageUrl.length>1">
+        <img :src="imageUrl">
+        <div class="image-preview-action">
+          <i @click="rmImage" class="el-icon-delete"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  // 预览效果见文章
+  export default {
+    name: 'cropAndUpload',
+    props: {
+      value: String,
+      previewSize: {
+        type: Object,
+        default: {
+          width: 200,
+          height: 200
+        }
+      }
+    },
+    computed: {
+      imageUrl() {
+        return this.value;
+      }
+    },
+    data() {
+      return {
+      };
+    },
+    methods: {
+      rmImage() {
+        this.emitInput('');
+      },
+      emitInput(val) {
+        this.$emit('input', val);
+      },
+      handleImageScucess(file) {
+        this.emitInput(file.files.file);
+      },
+      beforeUpload() {
+        return new Promise((resolve, reject) => {
+        });
+      }
+    }
+  };
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  @import "../../assets/css/mixin.scss";
+  .upload-container {
+    width: 100%;
+    position: relative;
+    @include clearfix;
+    .image-uploader {
+      width: 35%;
+      float: left;
+    }
+    .image-preview {
+      width: 200px;
+      height: 200px;
+      position: relative;
+      border: 1px dashed #d9d9d9;
+      float: left;
+      margin-left: 50px;
+      .image-preview-wrapper {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+      .image-preview-action {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        left: 0;
+        top: 0;
+        cursor: default;
+        text-align: center;
+        color: #fff;
+        opacity: 0;
+        font-size: 20px;
+        background-color: rgba(0, 0, 0, .5);
+        transition: opacity .3s;
+        cursor: pointer;
+        text-align: center;
+        line-height: 200px;
+        .el-icon-delete {
+          font-size: 36px;
+        }
+      }
+      &:hover {
+        .image-preview-action {
+          opacity: 1;
+        }
+      }
+    }
+    .image-app-preview {
+      width: 320px;
+      height: 180px;
+      position: relative;
+      border: 1px dashed #d9d9d9;
+      float: left;
+      margin-left: 50px;
+      .app-fake-conver {
+        height: 44px;
+        position: absolute;
+        width: 100%; // background: rgba(0, 0, 0, .1);
+        text-align: center;
+        line-height: 64px;
+        color: #fff;
+      }
+    }
+  }
+</style>
--- src/views/SwiperManager/add.vue
+++ src/views/SwiperManager/add.vue
@@ -4,9 +4,33 @@
       <sticky :className="'sub-navbar published'">
         <el-button v-loading="loading" style="margin-left: 10px;" type="success" @click="submitForm()">保存
         </el-button>
-        <el-button v-loading="loading" type="warning" @click="cancel()">取消</el-button>
-      
+        <el-button type="warning" @click="cancel()">取消</el-button>
       </sticky>
+      
+      <div class="createPost-main-container">
+        <el-form-item label="轮播图名称" prop="name">
+          <el-col :span="11">
+            <el-input v-model="postForm.name"></el-input>
+          </el-col>
+        </el-form-item>
+  
+        <el-form-item label="轮播图链接" prop="linkType">
+          <el-col :span="11" >
+            <el-select v-model="postForm.linkType" placeholder="请选择轮播图链接类型"  >
+              <el-option label="列表广告" :value="1"></el-option>
+              <el-option label="单品广告" :value="2"></el-option>
+              <el-option label="外部链接" :value="3"></el-option>
+            </el-select>
+          </el-col>
+        </el-form-item>
+  
+        <el-form-item label="轮播图设置" prop="linkType">
+          <el-col :span="24" >
+            <crop-and-upload v-model="postForm.image_uri" :preview-size="{width: 720, height: 288}"></crop-and-upload>
+          </el-col>
+        </el-form-item>
+        
+      </div>
     </el-form>
   
   </div>
@@ -14,21 +38,42 @@
 
 <script>
   import Sticky from '../../components/Sticky/index.vue';
+  import {CropAndUpload} from '../../components/ImageUpload';
+  
   export default {
     name: 'add-swiper',
     components: {
-      Sticky
+      Sticky,
+      CropAndUpload
     },
     data() {
       return {
+        postForm: {
+          name: '',
+          linkType: '',
+          image_uri: ''
+        },
+        rules: {
+          name: [
+            { required: true, message: '请输入轮播图名称', trigger: 'blur' },
+            { min: 5, max: 20, message: '长度在 5 到 20 个字符', trigger: 'blur' }
+          ],
+          linkType: [
+            { required: true, message: '请选择轮播图链接类型', trigger: 'change' }
+          ],
+          image_uri: [
+            { required: true, message: '请设置轮播图', trigger: 'blur' }
+          ]
+        },
+        loading: false
       };
     },
-    computed: {
-    },
+    computed: {},
     created() {
     },
     methods: {
-      submitForm() {},
+      submitForm() {
+      },
       cancel() {
         this.$emit('changView', 'list');
       }
@@ -37,13 +82,15 @@
 </script>
 <style rel="stylesheet/scss" lang="scss" scoped>
   @import "../../assets/css/mixin.scss";
-  .title-prompt{
+  
+  .title-prompt {
     position: absolute;
     right: 0px;
     font-size: 12px;
-    top:10px;
-    color:#ff4949;
+    top: 10px;
+    color: #ff4949;
   }
+  
   .createPost-container {
     position: relative;
     .createPost-main-container {
