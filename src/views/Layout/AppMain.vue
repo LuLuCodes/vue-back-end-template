9da75ee924ddb968c42445fb56b1d0d0b7fb1384
qianqing <qian.qing@aliyun.com>
2017-07-20 13:19:43 +0800

    init

--- src/views/Layout/AppMain.vue
+++ src/views/Layout/AppMain.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="app-main" style="min-height: 100%">
     <transition name="fade" mode="out-in">
-      <router-view :key="key"></router-view>
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
     </transition>
   </section>
 </template>
