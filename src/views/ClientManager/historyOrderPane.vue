bebd4dabf64e8c1c255a8969b2a51eaeafcf037f
qianqing <qian.qing@aliyun.com>
2017-07-20 14:54:20 +0800

    修改路由keep-alive，保证页面状态

--- src/views/ClientManager/historyOrderPane.vue
+++ src/views/ClientManager/historyOrderPane.vue
@@ -164,11 +164,11 @@
     },
     computed: {},
     created() {
+      this.getList();
     },
     activated() {
       // 当组件在 <keep-alive> 内被切换，它的 activated 和 deactivated 这两个生命周期钩子函数将会被对应执行
       // 数据加载应该放在此处
-      this.getList();
     },
     deactivated() {
       // 当组件在 <keep-alive> 内被切换，它的 activated 和 deactivated 这两个生命周期钩子函数将会被对应执行
