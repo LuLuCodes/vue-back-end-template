8a1b32ef25ad91dd8c187143925858d082db2985
qianqing <qian.qing@aliyun.com>
2017-07-21 09:20:20 +0800

    订单管理

--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,9 @@ import {AddGood, GoodList} from '../views/GoodManager';
 /* category manager */
 import CategoryManager from '../views/CategoryManager/index.vue';
 
+/* order manager */
+import {OrderList} from '../views/OrderManager';
+
 /* error page */
 import Err404 from '../views/Error/404.vue';
 import Err401 from '../views/Error/401.vue';
@@ -94,6 +97,16 @@ export const asyncRouterMap = [
       {path: 'good-manager', component: GoodList, name: '商品列表', meta: {keepAlive: true}},
       {path: 'add-good', component: AddGood, name: '新增商品', hidden: true, meta: {keepAlive: false}},
       {path: 'category-manager', component: CategoryManager, name: '商品分类设置', meta: {keepAlive: false}}
+    ]
+  },
+  {
+    path: '/order',
+    component: Layout,
+    redirect: '/order/order-manager',
+    name: '订单管理',
+    icon: 'fa-file-text',
+    children: [
+      {path: 'order-manager', component: OrderList, name: '订单列表', meta: {keepAlive: true}}
     ]
   },
   {path: '*', redirect: '/404', hidden: true}
---
+++ src/views/OrderManager/list.vue
@@ -0,0 +1,21 @@
+<template>
+</template>
+
+<script>
+  export default {
+    name: '',
+    components: {},
+    data() {
+      return {};
+    },
+    props: {},
+    computed: {},
+    created() {
+    },
+    filters: {},
+    methods: {}
+  };
+</script>
+<style scoped>
+
+</style>
