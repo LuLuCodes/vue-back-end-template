4237e9575f1a87ab7516de28ba67f57c242d661e
qianqing <qian.qing@aliyun.com>
2017-07-21 11:48:55 +0800

    订单管理

--- src/views/OrderManager/list.vue
+++ src/views/OrderManager/list.vue
@@ -1,93 +1,112 @@
 <template>
   <div class="app-container calendar-list-container">
-    <div class="filter-container">
-      <el-row>
-        <el-col :span="14">
-          <el-select clearable style="width: 100px" class="filter-item" v-model="listQuery.status" placeholder="状态"
-                     size="small">
-            <el-option v-for="item in orderStatus" :key="item.value" :label="item.label" :value="item.value">
-            </el-option>
-          </el-select>
-          
-          <el-input @keyup.enter.native="handleFilter" style="width: 230px;" class="filter-item"
-                    placeholder="请输入订单号" v-model="listQuery.orderId" size="small">
-          </el-input>
-
-          
-          <el-button class="filter-item" type="primary" v-waves icon="search" @click="handleFilter" size="small">搜索
-          </el-button>
-          
-          <!--高级搜索popover-->
-          <el-popover
-            ref="advancedSearchPopover"
-            placement="top-start"
-            width="1000"
-            trigger="click"
-            v-model="showAdvancedSearchPopover">
-            <el-form label-width="80px" label-position="right">
-              <el-row>
-                <el-col :span="10">
-                  <el-form-item label="关键词">
-                    <el-input size="small" placeholder="请输入订单号" style="width: 300px;"></el-input>
-                  </el-form-item>
-                </el-col>
-                <el-col :span="12">
-                  <el-form-item label="下单时间">
-                    <el-date-picker
-                      type="datetimerange"
-                      :picker-options="dateTimePickerOptions"
-                      placeholder="选择时间范围"
-                      align="right">
-                    </el-date-picker>
-                  </el-form-item>
-                </el-col>
-              </el-row>
+    <transition name="component-fade" mode="out-in">
+      <div class="filter-container" v-if="!selectOrders.length">
+        <el-row>
+          <el-col :span="14">
+            <el-select clearable style="width: 100px" class="filter-item" v-model="listQuery.status" placeholder="状态"
+                       size="small">
+              <el-option v-for="item in orderStatus" :key="item.value" :label="item.label" :value="item.value">
+              </el-option>
+            </el-select>
+        
+            <el-input @keyup.enter.native="handleFilter" style="width: 230px;" class="filter-item"
+                      placeholder="请输入订单号" v-model="listQuery.orderId" size="small">
+            </el-input>
+        
+        
+            <el-button class="filter-item" type="primary" v-waves icon="search" @click="handleFilter" size="small">搜索
+            </el-button>
+        
+            <!--高级搜索popover-->
+            <el-popover
+              ref="advancedSearchPopover"
+              placement="top-start"
+              width="1000"
+              trigger="click"
+              v-model="showAdvancedSearchPopover">
+              <el-form label-width="80px" label-position="right">
+                <el-row>
+                  <el-col :span="10">
+                    <el-form-item label="关键词">
+                      <el-input size="small" placeholder="请输入订单号" style="width: 300px;"></el-input>
+                    </el-form-item>
+                  </el-col>
+                  <el-col :span="12">
+                    <el-form-item label="下单时间">
+                      <el-date-picker
+                        type="datetimerange"
+                        :picker-options="dateTimePickerOptions"
+                        placeholder="选择时间范围"
+                        align="right">
+                      </el-date-picker>
+                    </el-form-item>
+                  </el-col>
+                </el-row>
+            
+                <el-row>
+                  <el-col :span="10">
+                    <el-form-item label="收货人信息">
+                      <el-input size="small" placeholder="请输入收货人/收货电话" style="width: 300px;"></el-input>
+                    </el-form-item>
+                  </el-col>
               
-              <el-row>
-                <el-col :span="10">
-                  <el-form-item label="收货人信息">
-                    <el-input size="small" placeholder="请输入收货人/收货电话" style="width: 300px;"></el-input>
-                  </el-form-item>
-                </el-col>
-  
-                <el-col :span="10">
-                  <el-form-item label="商品信息">
-                    <el-input size="small" placeholder="请输入商品名称/编码" style="width: 300px;"></el-input>
-                  </el-form-item>
-                </el-col>
-              </el-row>
-              
-              <el-row>
-                <el-col :span="24">
-                  <el-form-item label="订单状态">
-                    <el-checkbox size="small" :indeterminate="isIndeterminateStatus" v-model="checkAllStatus"
-                                 @change="handleCheckAllStatusChange">全选
-                    </el-checkbox>
-                    <el-checkbox-group v-model="checkedStatuss" @change="handleCheckedStatusChange"
-                                       style="display: inline-block;margin-left: 15px;">
-                      <el-checkbox size="small" v-for="status in orderStatus" :label="status.value" :key="status.value">
-                        {{status.label}}
+                  <el-col :span="10">
+                    <el-form-item label="商品信息">
+                      <el-input size="small" placeholder="请输入商品名称/编码" style="width: 300px;"></el-input>
+                    </el-form-item>
+                  </el-col>
+                </el-row>
+            
+                <el-row>
+                  <el-col :span="24">
+                    <el-form-item label="订单状态">
+                      <el-checkbox size="small" :indeterminate="isIndeterminateStatus" v-model="checkAllStatus"
+                                   @change="handleCheckAllStatusChange">全选
                       </el-checkbox>
-                    </el-checkbox-group>
-                  </el-form-item>
-                </el-col>
-              </el-row>
-              
-              <el-form-item>
-                <el-button type="primary" size="small">确定</el-button>
-                <el-button size="small" @click="showAdvancedSearchPopover = false">取消</el-button>
-              </el-form-item>
-            </el-form>
-          </el-popover>
-          <el-button class="filter-item" type="primary" v-waves icon="menu" v-popover:advancedSearchPopover size="small"
-                     style="margin-left: 0;">高级搜索
-          </el-button>
-        </el-col>
-      </el-row>
-    </div>
+                      <el-checkbox-group v-model="checkedStatuss" @change="handleCheckedStatusChange"
+                                         style="display: inline-block;margin-left: 15px;">
+                        <el-checkbox size="small" v-for="status in orderStatus" :label="status.value" :key="status.value">
+                          {{status.label}}
+                        </el-checkbox>
+                      </el-checkbox-group>
+                    </el-form-item>
+                  </el-col>
+                </el-row>
+            
+                <el-form-item>
+                  <el-button type="primary" size="small">确定</el-button>
+                  <el-button size="small" @click="showAdvancedSearchPopover = false">取消</el-button>
+                </el-form-item>
+              </el-form>
+            </el-popover>
+            <el-button class="filter-item" type="primary" v-waves icon="menu" v-popover:advancedSearchPopover size="small"
+                       style="margin-left: 0;">高级搜索
+            </el-button>
+          </el-col>
+        </el-row>
+      </div>
+      <div class="filter-container order-operation-meun" v-else>
+        <el-row>
+          <el-col :span="2">
+            已选中<span style="color: #13c19f">{{selectOrders.length}}</span>项
+          </el-col>
+          <el-col :span="1">
+            |
+          </el-col>
+          <el-col :span="2">
+            <i class="fa fa-truck fa-lg"></i> 确认发货
+          </el-col>
+          <el-col :span="2">
+            <i class="fa fa-trash fa-lg"></i> 取消订单
+          </el-col>
+        </el-row>
+      </div>
+    </transition>
+    
   
     <el-table :data="list" v-loading="listLoading" element-loading-text="拼命加载中" :height="tableHeight" border stripe fit
-              highlight-current-row style="width: 100%">
+              highlight-current-row style="width: 100%" @select="handleSelection" @select-all="handleSelectionAll">
       <el-table-column align="center"
                        type="selection"
                        width="55">
@@ -214,7 +233,8 @@
               picker.$emit('pick', [start, end]);
             }
           }]
-        }
+        },
+        selectOrders: []
       };
     },
     created() {
@@ -272,6 +292,12 @@
       },
       handleCreate() {
         this.jump({path: '/good/add-good'});
+      },
+      handleSelection(selection, row) {
+        this.selectOrders = selection;
+      },
+      handleSelectionAll(selection) {
+        this.selectOrders = selection;
       }
     },
     beforeRouteEnter (to, from, next) {
@@ -292,5 +318,10 @@
 <style>
   .order-manage-list.menu-popper-class {
     min-width: 60px !important;
+  }
+  
+  .order-operation-meun {
+    font-size: 14px;
+    color: #97a8be;
   }
 </style>
